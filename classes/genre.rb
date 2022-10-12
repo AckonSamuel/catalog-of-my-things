a7dae4ba384e3b841a053bf3f1f2ebd1b4657936
Samuel Ackon <ackons433@gmail.com>
2022-10-12 15:19:01 +0000

    Fix linter error

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,22 +1,21 @@
 require 'item'
 
-class Genre 
+class Genre
+  attr_accessor :name, :items
 
-    attr_accessor :name, :items
+  def initialize(name)
+    @id = rand(1...999)
+    @name = name
+    @items = []
+  end
 
-    def initialize(name)
-        @id = rand(1...999)
-        @name = name
-        @items = []
+  def add_item(items)
+    case items
+    when Item
+      items.add_genre(self)
+      @items << items
+    else
+      puts 'Class error: Must either be a book, game, music album, or movie'
     end
-
-    def add_item(items)
-        case items
-        when Item
-            items.add_genre(self)
-            @items << items
-        else
-            puts "Class error: Must either be a book, game, music album, or movie"
-        end
-    end
+  end
 end
