bbaf54be0c3c280d548c4982adab9811eae52c04
Samuel Ackon <ackons433@gmail.com>
2022-10-12 15:10:44 +0000

    Add required method

--- classes/genre.rb
+++ classes/genre.rb
@@ -2,12 +2,21 @@ require 'item'
 
 class Genre 
 
+    attr_accessor :name, :items
+
     def initialize(name)
         @id = rand(1...999)
         @name = name
         @items = []
     end
 
-    def add_items
+    def add_item(items)
+        case items
+        when Item
+            items.add_genre(self)
+            @items << items
+        else
+            puts "Class error: Must either be a book, game, music album, or movie"
+        end
     end
 end
