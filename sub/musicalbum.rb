11ad247e1dd57375352426a39561839bd37b0a47
Samuel Ackon <ackons433@gmail.com>
2022-10-12 13:34:58 +0000

    Fix linter errors

--- sub/musicalbum.rb
+++ sub/musicalbum.rb
@@ -1,34 +1,36 @@
 require 'item'
 
 class MusicAlbum < Item
+  def initialize(on_spotify: true)
+    super(genre, author, source, label, publish_date)
+    @on_spotify = on_spotify
+    @music_arr = []
+  end
 
-    def initialize(on_spotify: true)
-        super(genre, author, source, label, publish_date)
-        @on_spotify = on_spotify
-        @music_arr = []
+  def add_album
+    @music_arr.push({
+                      genre: @genre,
+                      author: @author,
+                      label: @label,
+                      source: @source,
+                      publish_date: @publish_date,
+                      on_spotify: @on_spotify
+                    })
+  end
+
+  def list_album
+    return unless @music_arr.length
+
+    @music_arr.each do |piece|
+      piece.each do |key, value|
+        puts "\n{#{key} : #{value}},\n"
+      end
     end
+  end
 
-    def add_album
-        @music_arr.push({
-            genre: @genre,
-            author: @author,
-            label: @label,
-            source: @source,
-            publish_date: @publish_date,
-            on_spotify: @on_spotify,
-        })
-    end
+  private
 
-    def list_album
-        @music_arr.each do |piece|
-            piece.each do |key, value| puts "\n{#{key} : #{value}},\n"
-            end
-        end if @music_arr.length
-    end
-
-    private
-
-    def can_be_archived?
-     @archived == true && on_spotify == true
-    end
+  def can_be_archived?
+    @archived == true && on_spotify == true
+  end
 end
