93d033d8ac436fbc31fd441be1c12e87a21b487f
Samuel Ackon <ackons433@gmail.com>
2022-10-13 15:00:30 +0000

    Create add_album method

--- app.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'app_album'
-require_relative 'app_game'
-require_relative 'app_book' 
-
-class App
-     
-end
--- app_album.rb
+++ app_album.rb
@@ -1,28 +1,26 @@
+class AppAlbum
+  attr_accessor :albums
 
-class AppAlbum 
-    attr_accessor :albums
-    def initialize
-        @albums = []
-    end
-
-def add_album
+  def initialize
+    @albums = []
+  end
+git 
+  def add_album
     puts 'Creating a new music album'
     puts
     print 'Is the album on Spotify[Y/N]: '
-    spot  = gets.chomp.downcase
-    puts 'Sorry, invalid input' unless spot == 'y' || spot == 'n'
+    spot = gets.chomp.downcase
+    puts 'Sorry, invalid input' unless %w[y n].include?(spot)
     light = false
-    spot == 'y' ? light = true : light
+    spot == 'y' ? !light : light
 
     puts 'Input Published date e.g. 2021-01-01'
     print 'Date: '
     date = gets.chomp
     puts 'Sorry, You input invalid date' unless date.match?(/\d{4}-\d{2}-\d{2}/)
-   
+
     new_album = MusicAlbum.new(date, on_spotify: light)
 
     @albums << new_album
+  end
 end
-
-end
-
